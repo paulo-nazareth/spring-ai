f0e18b485b84c1a38a2120681aca5bca69663c11
Paulo Henrique de Jesus Nazareth <paulo.nazareth@sesicni.com.br>
2026-03-30 08:55:38 -0300

    #05 - Criando Projeto Angular

--- angular-ai/src/app/app.ts
+++ angular-ai/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-ai');
+  protected readonly title = 'AI - Spring Angular';
 }
