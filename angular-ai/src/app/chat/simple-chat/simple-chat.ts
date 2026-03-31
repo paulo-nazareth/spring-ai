eac21045d5a6956badca36bd19d7d531d3ff3762
Paulo Henrique de Jesus Nazareth <paulo.nazareth@sesicni.com.br>
2026-03-31 08:29:18 -0300

    #07 - Clone ChatGPT com API

--- angular-ai/src/app/chat/simple-chat/simple-chat.ts
+++ angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { scrollUp, scrollDown } from './../../../../node_modules/ansi-escapes/base.d';
+import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -6,7 +7,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbar } from '@angular/material/toolbar';
 import { NgClass } from "@angular/common";
-import { timeout } from 'rxjs';
+import { timeout, catchError, of, throwError } from 'rxjs';
+import { ChatService } from '../chat-service';
 
 @Component({
   selector: 'app-simple-chat',
@@ -16,7 +18,15 @@ import { timeout } from 'rxjs';
 })
 export class SimpleChat {
 
+  @ViewChild('chatHistory') 
+  private chatHistory!: ElementRef;
+
+  private chatService = inject(ChatService);
+
   userInput = '';
+  isLoading = false;
+
+  local = false;
 
   messages = signal([
     { text: 'Olá, como posso te ajudar hoje?', isBot: true }
@@ -24,11 +34,32 @@ export class SimpleChat {
 
   sendMessage(){
     this.trimUserMessage();
-    if (this.userInput != ''){
+    if (this.userInput != '' && !this.isLoading){
       this.updateMessages(this.userInput, false);
-      this.userInput = '';
-      this.simulateResponse();
+      this.isLoading = true;
+      if(this.local){
+        this.simulateResponse();
+      } else {
+        this.sendChatMessage();
+      }
     }
+  }
+
+  private sendChatMessage(){
+    this.chatService.sendChatMassage(this.userInput)
+          .pipe(
+            catchError(() => {
+              this.updateMessages('Desculpe, não posso processar sua mensagem no momento!', true);
+              this.isLoading = false;
+              return throwError(() => new Error('Um erro ocorreu ao mandar a mensagem'))
+              // return of();
+            })
+          )
+          .subscribe(response => {
+            this.updateMessages(response.message, true);
+            this.userInput = '';
+            this.isLoading = false;
+          });
   }
 
   private trimUserMessage(){
@@ -37,12 +68,20 @@ export class SimpleChat {
 
   private updateMessages(text: string, isBot = false){
     this.messages.update( messages => [ ...messages, { text: text, isBot: isBot }] );
+    this.scrollToBottom();
   }
 
   private simulateResponse(){
     setTimeout(() => {
       const response = 'Esta é uma resposta simulada do Chat AI.';
       this.updateMessages(response, true);
-    }, 2000);
+      this.isLoading = false;
+    }, 5000);
+  }
+
+  private scrollToBottom(){
+    try {
+      this.chatHistory.nativeElement.scrollTop = this.chatHistory.nativeElement.scrollHeight;
+    } catch(err) {}
   }
 }
---
+++ spring-ai/src/main/java/com/spring_ai/chat/ChatMessage.java
@@ -0,0 +1,5 @@
+package com.spring_ai.chat;
+
+public record ChatMessage(String message) {
+
+}
